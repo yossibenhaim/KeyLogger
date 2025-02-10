ddc84d0642e65529ccc67f32be37fdfc8007b21a
shale <s0534156555@gmail.com>
2025-02-10 17:06:10 +0200

    encryption

--- .venv/KeyLogger.py
+++ .venv/KeyLogger.py
@@ -1,2 +1,12 @@
 class Key_Logger:
-    pass
+    def encryption(self,array):
+        array_encryption = []
+        for word in array:
+            nem = ord(word)+2
+            nem = (nem ** 2)//65
+            if nem > 122:
+                nem -= 57
+            array_encryption.append(chr(nem))
+        return array_encryption
+
+
